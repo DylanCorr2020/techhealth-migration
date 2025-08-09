d474980f279c5a12bfcdd76505083a3d61507c9d
Dylan Corr <dylancorr.g@gmail.com>
2025-08-09 17:54:19 +0100

    add RDS MySQL in private subnet and EC2 instances in public subnet

--- techhealth-infrastructure/lib/techhealth-infrastructure-stack.ts
+++ techhealth-infrastructure/lib/techhealth-infrastructure-stack.ts
@@ -1,7 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as rds from 'aws-cdk-lib/aws-rds';
+
 
 export class TechhealthInfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -25,13 +26,43 @@ export class TechhealthInfrastructureStack extends cdk.Stack {
         }
       ]
     })
-    
-    // Output the VPC ID after deployment
+
+    //Create two EC2 Instances in public subnet
+  
+    const instance1 = new ec2.Instance(this, 'PublicEC2-1',{
+     vpc: vpc,
+     vpcSubnets:{
+    subnetType: ec2.SubnetType.PUBLIC
+    },
+
+    instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+    machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+
+   })
+
+
+
+
+
+   //Create RDS instance in private subnet 
+   const dbInstance = new rds.DatabaseInstance(this, 'PrivateRDS-1', {
+     engine: rds.DatabaseInstanceEngine.mysql({ version: rds.MysqlEngineVersion.VER_8_0 }),
+     vpc,
+     vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+     instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
+     allocatedStorage: 20,
+     removalPolicy: cdk.RemovalPolicy.DESTROY,
+   })
+
+   
+  
+
+     // Output the VPC ID after deployment
     new cdk.CfnOutput(this,'VpcId', {
       value: vpc.vpcId,
       description: 'VPC UID'
     })
- }
 
+ }
 }
 
