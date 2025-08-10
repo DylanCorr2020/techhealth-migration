9cc07a947f32fe9184667e8ed1ae0efbc470ef3e
Dylan Corr <dylancorr.g@gmail.com>
2025-08-10 07:47:40 +0100

    split infrastructure into VPC and EC2 stacks

---
+++ techhealth-infrastructure/lib/vpc-stack.ts
@@ -0,0 +1,41 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+
+export class VPCStack extends cdk.Stack {
+
+  //vpc can be read by other stacks 
+  public readonly vpc: ec2.Vpc
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create a new VPC with 2 Availability Zones
+    this.vpc = new ec2.Vpc(this, 'Vpc', {
+    maxAzs: 2,
+      
+      // Define subnets to be created in the VPC
+      subnetConfiguration: [
+        {
+          name: 'Public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24
+        },
+        {
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24
+        }
+      ]
+    })
+
+     // Output the VPC ID after deployment
+    new cdk.CfnOutput(this,'VpcId', {
+      value: this.vpc.vpcId,
+      description: 'VPC UID'
+    })
+
+ }
+}
+
